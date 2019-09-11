95e5dfcb5700b9db334aa55d62d8450c99c5aae6
Stephanie Hooker <stephanieh799@gmail.com>
2019-09-11 13:48:36 -0600

    added houses.vue

---
+++ src/views/Houses.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="houses">
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'houses',
+  data(){
+    return {}
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
